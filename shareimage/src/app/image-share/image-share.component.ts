9a07c4d69c358e22b3f8cb1c063e7ed98921f885
ThibaudMallet <thibaud.mallet@hotmail.fr>
2022-10-12 19:46:13 +0200

    Custom du premier composant de manière dynamique

--- shareimage/src/app/image-share/image-share.component.ts
+++ shareimage/src/app/image-share/image-share.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image-share',
   templateUrl: './image-share.component.html',
   styleUrls: ['./image-share.component.scss']
 })
-export class ImageShareComponent {}
+export class ImageShareComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  likes!: number;
+  imageUrl!: string;
+
+  ngOnInit()
+  {
+    this.title = "Archibald";
+    this.description = "Photo de Archibald";
+    this.createdDate = new Date();
+    this.likes = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+}
