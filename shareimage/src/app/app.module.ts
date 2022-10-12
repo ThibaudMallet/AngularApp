441c9fde5c01622301a1f94ee78163b23f0e302d
ThibaudMallet <thibaud.mallet@hotmail.fr>
2022-10-12 17:05:14 +0200

    Creation du premier composant

--- shareimage/src/app/app.module.ts
+++ shareimage/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ImageShareComponent } from './image-share/image-share.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageShareComponent
   ],
   imports: [
     BrowserModule
---
+++ shareimage/src/app/image-share/image-share.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-share',
+  templateUrl: './image-share.component.html',
+  styleUrls: ['./image-share.component.scss']
+})
+export class ImageShareComponent {}
